6351b1612d4cf48b76ca819bc87d30308b75a9da
vitorcapovilla <vitoraugustocapovila@gmail.com>
2025-02-06 01:31:38 -0300

    Possibilidade de criação de blocos e itens; Criado a Safira, bloco de safira e bloco de minério de safira; Craft da Safira para o bloco o vise-versa realizado

--- src/main/java/net/vari/varicraft/VariCraft.java
+++ src/main/java/net/vari/varicraft/VariCraft.java
@@ -1,6 +1,7 @@
-package net.vari.vaicraft;
+package net.vari.varicraft;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -11,6 +12,8 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.vari.varicraft.block.ModBlocks;
+import net.vari.varicraft.item.ModItems;
 import org.slf4j.Logger;
 
 @Mod(VariCraft.MOD_ID)
@@ -20,6 +23,13 @@ public class VariCraft {
 
     public VariCraft(FMLJavaModLoadingContext context) {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        //Inicialização de Items
+        ModItems.register(modEventBus);
+
+        //Inicialização de Blocos
+        ModBlocks.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
         MinecraftForge.EVENT_BUS.register(this);
         modEventBus.addListener(this::addCreative);
@@ -29,6 +39,11 @@ public class VariCraft {
     }
 
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SAFIRA);
+            event.accept(ModBlocks.MINERIO_DE_SAFIRA);
+            event.accept(ModBlocks.BLOCO_DE_SAFIRA);
+        }
     }
 
     @SubscribeEvent
---
+++ src/main/java/net/vari/varicraft/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.vari.varicraft.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.vari.varicraft.VariCraft;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, VariCraft.MOD_ID);
+
+    public static final RegistryObject<Item> SAFIRA = ITEMS.register("safira",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
